b64369e7015a92637de22d71f3a483e38a410673
italo pessan <italopessan@gmail.com>
2023-11-28 10:35:03 -0300

    models

---
+++ SalesWebMvc.Business/Models/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Business.Models
+{
+    public enum SaleStatus : int
+    {
+        PENDING = 0,
+        BILLED = 1,
+        CANCELED = 2,
+    }
+}
---
+++ SalesWebMvc.Business/Models/SalesRecord.cs
@@ -0,0 +1,22 @@
+﻿namespace SalesWebMvc.Business.Models
+{
+    public class SalesRecord
+    {
+        public int id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amounth { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() { }
+       
+        public SalesRecord(int id, DateTime date, double amounth, SaleStatus status, Seller seller)
+        {
+            this.id = id;
+            Date = date;
+            Amounth = amounth;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
